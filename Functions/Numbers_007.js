712037b1e822c9d21502c76591617f6aa9e0ca68
m.kochnev <maksimkochnev563@gmail.com>
2022-07-06 13:15:23 +0300

    mission complite

--- Functions/Numbers_007.js
+++ Functions/Numbers_007.js
@@ -1,26 +1,33 @@
 function Sort(mas) {
     let accordance = []
-    let accordance_true = [0, 0, 7]
 
     for (let i in mas) {
-        if (accordance == accordance_true) {
-            break
-        }
         if (mas[i] === 0) {
-            if (mas[Number(i) + 1] === 0 && mas[Number(i) + 2] === 7) {
-                accordance.push(mas[i])
-                accordance.push(mas[Number(i) + 1])
-                accordance.push(mas[Number(i) + 2])
-            }
+            accordance.push(mas[i])
+        }
+        if (mas[i] === 7) {
+            accordance.push(mas[i])
         }
     }
-    if(accordance[0] == accordance_true[0] && accordance[1] == accordance_true[1] && accordance[2] == accordance_true[2]) {
+    console.log(accordance)
+    let accordance_true = []
+    for(let i in accordance) {
+        if (accordance[Number(i) + 1] === 0 && accordance[Number(i) + 2] === 7) {
+            accordance_true.push(accordance[i])
+            accordance_true.push(accordance[Number(i) + 1])
+            accordance_true.push(accordance[Number(i) + 2])
+        }
+    }
+    console.log(accordance_true)
+
+    let answer_true = [0,0,7]
+    if(accordance_true[0] == answer_true[0] && accordance_true[1] == answer_true[1] && accordance_true[2] == answer_true[2]){
         console.log(true)
         return true
     } else {
         console.log(false)
         return false
     }
-
 }
-Sort([/*ваш массив*/])
+
+Sort([1, 1, 4, 0, 6, 7, 0, 1, 1, 0, 4])
--- Functions/staff.js
+++ Functions/staff.js
@@ -1,12 +1,15 @@
 function Staff(mas){
     let finally_stafs = {}
     for(let i in mas){
-        console.log(mas[i].split(' ')[1][0])
-        finally_stafs[mas[i].split(' ')[1][0]] = {[mas[i][0]]: [mas[i]]}
+        if(!finally_stafs[mas[i].split(' ')[1][0]]) {
+            finally_stafs[mas[i].split(' ')[1][0]] = {[mas[i][0]]: [mas[i]]}
+        } else {
+            finally_stafs[mas[i].split(' ')[1][0]+ ' '] = {[mas[i][0]]: [mas[i]]}
+        }
     }
     console.log(finally_stafs)
 }
 
-Staff(/*вам массив*/)
+Staff(['Иван Сергеев', 'Инна Серова', "Петр Алексеев", "Илья Иванов",])
 
 
